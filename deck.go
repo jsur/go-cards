976b0773623c506cca28a0c4b31d3769f3c8b3e9
julius.suominen <julius.suominen@gmail.com>
2021-02-21 19:11:23 +0200

    add newDeck

---
+++ deck.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+// receiver function!
+// any variable of type "deck" gets access to print function
+/*
+	by convention, the receiver is usually referred to with a one-letter
+	abbreviation that matches its type (deck)
+*/
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 // files in the same package can freely call functions from each other
 package main
 
-import "fmt"
-
 func main() {
-	// := tells the compiler to infer type
-	// used only when a new variable is defined
-	card := "Ace of Spades"
-	fmt.Println(card)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
